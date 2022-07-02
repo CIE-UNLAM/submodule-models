912742f705977164570f5eb37049fb66d36d42ce
elancuba <lancuba.emiliano@gmail.com>
2022-07-02 10:44:30 -0300

    add efector model

---
+++ efector.ts
@@ -0,0 +1,45 @@
+import {DataTypes, Model} from "sequelize";
+import {DBManager} from "../utils/db";
+
+export class Efector extends Model {
+    declare institutionName: string
+    declare administrativeArea: string
+    declare attentionLevel: number
+    declare availableCityList:  Array<string>
+    declare active: boolean
+}
+
+Efector.init({
+    // Model attributes are defined here
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    institutionName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    administrativeArea: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    attentionLevel: {
+        type: DataTypes.INTEGER,
+        defaultValue: 1,
+        allowNull: false
+    },
+    availableCityList: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: true
+    },
+    active: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+        allowNull: false
+    }
+}, {
+    // Other model options go here
+    sequelize: DBManager.getInstance(), // We need to pass the connection instance
+    modelName: 'Efector' // We need to choose the model name
+});
