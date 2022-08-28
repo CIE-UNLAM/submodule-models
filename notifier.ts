e845a369bd992e1e62aceac65e2f8a13049d7a9c
Jonathan Borda <jonathanmatiasborda@gmail.com>
2022-08-28 17:02:03 -0300

    add notifier DTO

---
+++ notifier.ts
@@ -0,0 +1,9 @@
+export interface NotifierDTO {
+    title: string;
+    body: string;
+    previousRisk: number,
+    currentRisk: number,
+    socialRisk: number,
+    gestacionalWeekStart: number,
+    gestacionalWeekEnd: number,
+}
