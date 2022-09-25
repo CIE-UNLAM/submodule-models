3211a70ac1c7ab1f8db908d199685d4f23ee76a5
Jonathan Borda <jonathanmatiasborda@gmail.com>
2022-09-25 17:49:18 -0300

    change days of pregnancy

--- patient.ts
+++ patient.ts
@@ -38,10 +38,8 @@ export class Patient extends Model {
     static GuardAssistance: HasMany<Patient, GuardAssistance>;
 
     static calculateFPP(FUM: Date): Date {
-        let FPP = new Date();
-        FPP.setDate(FUM.getDate() + 10);
-        FPP.setMonth(FUM.getMonth() - 3);
-        FPP.setFullYear(FUM.getFullYear() + 1);
+        let FPP = new Date(FUM);
+        FPP.setDate(FUM.getDate() + 283);
         return FPP;
     }
 }
